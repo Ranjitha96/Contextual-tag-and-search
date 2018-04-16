059f798d25712ddf72071c0326f363c28a968456
sanair96 <sannair96@gmail.com>
2018-04-16 11:36:55 +0530

    added weights

--- source.py
+++ source.py
@@ -13,40 +13,77 @@ REQ = NOUNS + VERBS + PRONOUNS
 
 posToWN = {}
 for noun in NOUNS:
-	posToWN[noun] = wn.NOUN
+    posToWN[noun] = wn.NOUN
 for verb in VERBS:
-	posToWN[verb] = wn.VERB
+    posToWN[verb] = wn.VERB
+
+
+def tf(word, word_list):
+    return word_list.count(word)
+
+
+def score(word, word_list):
+    value = 0.0
+    for i, j in enumerate(word_list):
+        if j == word:
+            value += float(1 - (i / float(len(word_list))))
+    return value
+
 
 text = raw_input()
 token = nltk.word_tokenize(text)
 ps = PorterStemmer()
 tags = nltk.pos_tag(token)
-filtered_tags = {w:s for w, s in tags if w not in stopword and s in REQ}
-#print(filtered_tags)
-stemmed = {ps.stem(w): w for w in filtered_tags.keys()}
-#print(stemmed)
-
+filtered_wordsToTags = {w: s for w, s in tags if w not in stopword and s in REQ}
+# print(filtered_tags)
+stemmedToWords = {ps.stem(w): w for w in filtered_wordsToTags.keys()}
+# print(stemmed)
+print(stemmedToWords)
 synonymToStemmedWord = {}
 relatedWordSet = {}
+freq_count = {word: tf(word, token) for word in token}
+freq_score = {word: score(word, token) for word in token}
+print (freq_count)
+print(freq_score)
 
-for word in stemmed.keys():
-	print(word + " Stemmed")
-	if filtered_tags[stemmed[word]] not in PRONOUNS:
-		print(word + " Not Pronoun")
-		synset = wn.synsets(word, pos=posToWN[filtered_tags[stemmed[word]]])
-		for entry in synset:
-			print(str(entry) + " synset entry")
-			for synonym in entry.lemma_names():
-				print(synonym + " synonym")
-				if synonym not in synonymToStemmedWord:
-					print(word + " " + synonym + " New entry")
-					synonymToStemmedWord[synonym] = word
-					relatedWordSet[word] = []
-				else:
-					print(word + " added to " + synonymToStemmedWord[synonym])
-					relatedWordSet[synonymToStemmedWord[synonym]].append(word)
-
+for word in stemmedToWords.keys():
+    # print(word + " Stemmed")
+    if filtered_wordsToTags[stemmedToWords[word]] not in PRONOUNS:
+        # print(word + " Not Pronoun")
+        synset = wn.synsets(word, pos=posToWN[filtered_wordsToTags[stemmedToWords[word]]])
+        flag = False
+        for entry in synset:
+            # print(str(entry) + " synset entry")
+            for synonym in entry.lemma_names():
+                # print(synonym + " synonym")
+                if synonym not in synonymToStemmedWord:
+                    # print(word + " " + synonym + " New entry")
+                    synonymToStemmedWord[synonym] = word
+                    if word not in relatedWordSet:
+                        relatedWordSet[word] = []
+                else:
+                    print(synonymToStemmedWord[synonym] + ' ' + word)
+                    if synonymToStemmedWord[synonym] != word:
+                        # print(word + " added to " + synonymToStemmedWord[synonym])
+                        relatedWordSet[synonymToStemmedWord[synonym]].append(word)
+                    flag = True
+                    break
+            if flag:
+                break
 print(relatedWordSet)
+
+rootWordFrequency = {}
+rootWordWeight = {}
+
+for root, list in relatedWordSet.iteritems():
+    rootWordFrequency[root] = freq_count[stemmedToWords[root]]
+    rootWordWeight[root] = freq_score[stemmedToWords[root]]
+    for word in list:
+        rootWordFrequency[root] += freq_count[stemmedToWords[word]]
+        rootWordWeight[root] += freq_score[stemmedToWords[word]]
+print(rootWordFrequency)
+print ("hello\n")
+print(rootWordWeight)
 
 # zts = zip(tags,stemmed)
 # print zts
@@ -56,12 +93,12 @@ print(relatedWordSet)
 # 	if tags[stemmed.index(stem)]
 # count = {}
 
-#def mapper(wn_pos, nltk_pos):
+# def mapper(wn_pos, nltk_pos):
 
 
-#list_pos = NOUNS + VERBS + ADJS + ADVS
-#print
-#list_pos
+# list_pos = NOUNS + VERBS + ADJS + ADVS
+# print
+# list_pos
 
 #
 # for stem in stemmed:
